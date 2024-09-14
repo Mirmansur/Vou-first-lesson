e46b0d2f6760d336f53f4b64fbcd8b3c483ef4ca
Mirmansur <147687431+Mirmansur@users.noreply.github.com>
2024-09-14 13:55:35 +0500

    last

--- src/components/move-list-iems/MoveListItems.vue
+++ src/components/move-list-iems/MoveListItems.vue
@@ -1,14 +1,12 @@
 <template>
   <ul>
-    <li
-      class="list-group-item flex justify-between border-b-slate-900 :last-chaild border-b-none"
-    >
+    <li class="list-group-item flex justify-between border-b-slate-900">
       <span class="list-group-itme-label text-xl cursor-pointer w-[550px]"
         >Omar</span
       >
       <input
         type="number"
-        class="lits-group-item-label text-xl border-none outline-none text-center"
+        class="lits-group-item-input text-xl border-none outline-none text-center"
         defaultValue="811"
       />
       <div class="flex justify-center items-center">
@@ -22,7 +20,9 @@
           class="btn-trash btn-sm w-[35px] h-[35px] m-1 border-none bg-zinc-300 rounded-md text-xl text-center"
           ><i class="fas fa-trash text-[#e5383b]"></i
         ></butto>
-        <i class="fas fa-star"></i>
+        <i
+          class="fas fa-star w-[35px] h-[35px] text-center text-xl text-[#ffd700]"
+        ></i>
       </div>
     </li>
   </ul>
@@ -33,5 +33,18 @@ export default {};
 <style scoped>
 .list-group-item:last-child {
   border-bottom: none;
+}
+.list-group-item .fa-star {
+  transition: 0.3s all;
+  transform: translate(30px);
+  opacity: 0;
+}
+.list-group-item.like .fa-star {
+  opacity: 1;
+  transform: translate(0);
+}
+.list-group-item.favourute .lits-group-item-label,
+.list-group-item.favourute .lits-group-item-input {
+  color: #e09f3e;
 }
 </style>
